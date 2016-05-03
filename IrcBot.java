9ca39ba344d0a3894be2196451a368fb9d666bad
DarkFire21 <per.gkolias@gmail.com>
2016-05-03 18:10:55 +0300

    dummy

--- IrcBot.java
+++ IrcBot.java
@@ -1,89 +1,94 @@
-/** Lightweight IRC Client based on PIRC API   * Copyright (C) 2014 Author  * Dayne Sorvisto  */ 
+/**
+ * Lightweight IRC Client based on PIRC API   * Copyright (C) 2014 Author  * Dayne Sorvisto
+ */
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-public class IrcBot extends PircBot
-{
+import org.jibble.pircbot.PircBot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IrcBot extends PircBot {
     public ChatApp gui;
     // instance variable contains reference to GUI     public String current = "";
     //Stores the current channel     public String channel = "";
     //Stores the default channel     public String nick = " ";
-    //Enter a nickname to use on server     public List&lt;
-    String&gt;
-    windows = new ArrayList&lt;
-    String&gt;
-    ();
-    /**      * This method gets topic and passes it as a string to the GUI      */     @Override     protected void onTopic(String channel, String topic, String setBy, long date, boolean changed)
-    {
+    //Enter a nickname to use on server
+    public List<String> windows;
+    private String current;
+    private String nick;
+    private  String channel;
+
+    /**
+     * This method gets the topic and passes it as a string to the GUI.
+     * */
+    @Override
+    protected void onTopic(String channel, String topic, String setBy, long date, boolean changed) {
         gui.appendTopic(topic);
+
     }
-    /**      * This method receives the message string received from the IRC server and      * parses various IRC commands according to RFC specification      */     public boolean commands(String msg)
-    {
-        if (msg.startsWith("/join"))
-        {
+
+    /** This method receives the message string received from the IRC server and
+     * parses various IRC commands according to RFC specification
+     * */
+    public boolean commands(String msg) {
+        if (msg.startsWith("/join")) {
             msg = msg.replaceAll("/join", "");
             channel = msg.trim();
             windows.add(channel);
             this.current = channel;
             this.joinChannel(channel);
             return true;
-        }
-        else if (msg.startsWith("/nick"))
-        {
+        } else if (msg.startsWith("/nick")) {
             msg = msg.replaceAll("/nick", "");
             nick = msg.trim();
             this.changeNick(nick);
             return true;
-        }
-        else if (msg.startsWith("/window"))
-        {
+        } else if (msg.startsWith("/window")) {
             msg = msg.replaceAll("/window", "");
             channel = msg.trim();
             windows.add(channel);
             this.current = channel;
             this.joinChannel(channel);
             return true;
-        }
-        else if (msg.startsWith("/part"))
-        {
+        } else if (msg.startsWith("/part")) {
             this.partChannel(this.channel);
             windows.remove(this.channel);
             return true;
-        }
-        else if (msg.startsWith("/print"))
-        {
-            for (String s : windows)
-            {
+        } else if (msg.startsWith("/print")) {
+            for (String s : windows) {
                 System.out.println(s);
             }
             return true;
-        }
-        else if (msg.startsWith("/nick"))
-        {
+        } else if (msg.startsWith("/nick")) {
             msg = msg.replaceAll("/nick", "");
             this.nick = msg.trim();
             return true;
-        }
-        else if (msg.startsWith("/users"))
-        {
+        } else if (msg.startsWith("/users")) {
             gui.listUsers(this.getUsers(current));
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
-    /**      * Receives a message from the server and passes it to the GUI      */     @Override     public void onMessage(String channel, String sender, String login, String hostname, String message)
-    {
+
+    /**      * Receives a message from the server and passes it to the GUI      */
+    @Override
+    public void onMessage(String channel, String sender, String login, String hostname, String message) {
         gui.appendMessage(sender + ": " + message);
     }
-    /**      * Public constructor for IrcBot class takes one parameter      *      * @Parameter gui is a reference to our GUI operating in the background      */     public IrcBot(ChatApp gui)
-    {
+
+    /**
+     * Public constructor for IrcBot class takes one parameter
+     * @Parameter gui is a reference to our GUI operating in the background
+     * */
+    public IrcBot(ChatApp gui) {
         this.gui = gui;
         //we need to pass reference to our gui to this instance variable         gui.setDisplay(true);
         gui.listen(this);
+        windows = new ArrayList<>();
+    }
+
+    public String getNick() {
+        return nick;
     }
 }
